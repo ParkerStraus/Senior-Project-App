9f2c731b865d08b411907dab6cd546f2d8d61bc3
ogsiley <112645312+ogsiley@users.noreply.github.com>
2023-05-04 21:01:05 -0500

    silas
    silas

--- TimelyChefs/app/src/main/java/com/example/timelychefs/Settings.java
+++ TimelyChefs/app/src/main/java/com/example/timelychefs/Settings.java
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Settings extends AppCompatActivity {
     private Button General;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         General=findViewById(R.id.GeneralPage);
+        button = (Button) findViewById(R.id.HomeP);
         General.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +23,15 @@ public class Settings extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain();
+            }
+        });
+    }
+    public void openMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
